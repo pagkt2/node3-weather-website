123f90ec42defd5e46aa9ef8f0e1a4996bf2a7ac
paulgoergen <paulgoergen@paulgoergensair.lan>
2020-01-24 23:20:27 -0800

    high and low temps in home page

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location.', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.PrecipProbability + ' % chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.PrecipProbability + ' % chance of rain.  A high of ' + body.daily.data[0].temperatureHigh + ' degrees and a low of ' + body.daily.data[0].temperatureLow + ' degrees.')
         }
     })
 
